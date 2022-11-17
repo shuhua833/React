05de289e87130f4a2f8f91b1478c75b37a96ba63
Swechha Shrestha <shresthashuhua17@gmail.com>
2022-11-17 14:06:43 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <form className="form">
+    <h3>User Registration Form</h3>
+    <div className="main-div">
+        <label for="fname">Your Firstname</label><br></br>
+        <input type="text" placeholder="Firstname"/><br></br>
+
+        <label for="Lname">Your Lastname</label><br></br>
+        <input type="text" placeholder="Lname"/><br></br>
+
+        <label for="address">Your Address</label><br></br>
+        <input type="text" placeholder="Address"/><br></br>
+
+        <label for="location">Your Location</label><br></br>
+        <input type="text" placeholder="Address"/><br></br>
+
+        <label for="date">Your Date of birth</label><br></br>
+        <input type="date" placeholder="Date of birth"/><br></br>
+
+        <label for="gender">Your Gender</label><br></br>
+        <select name="gender">
+            <option value="Male">Male</option>
+            <option value="female">Female</option>
+        </select><br></br>
+
+        <label for="pic">Upload a photo</label>
+        <input type="file"/><br></br>
+        <div className="btn">
+            <button type="submit" className="submit"> Submit</button>
+            <button type="submit" className="cancel"> Cancel </button>
+        </div>
     </div>
+</form>
+</div>
   );
 }
 
